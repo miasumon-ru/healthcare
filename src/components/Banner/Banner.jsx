8014a0f4bd29218c59f30b8da111a0d54990af75
miasumon <miasumon.ru@gmail.com>
2024-07-03 13:42:09 +0600

    banner section completed

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+
+
+import headerBanner from '../../assets/Rectangle 5.png'
+
+const Banner = () => {
+    return (
+        <div className='mt-8'>
+
+            <img src={headerBanner} alt="" />
+            
+        </div>
+    );
+};
+
+export default Banner;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Banner from "../../components/Banner/Banner";
 import Navbar from "../../components/Navbar/Navbar";
 
 
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
 
             <Navbar></Navbar>
+            <Banner></Banner>
 
             
         </div>
