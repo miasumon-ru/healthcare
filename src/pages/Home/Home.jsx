1d5a770a920827df9b43f5b200a01753a76d00f3
miasumon <miasumon.ru@gmail.com>
2024-07-03 13:20:50 +0600

    initialized the healthcare project with necessary files

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+
+            <Navbar></Navbar>
+
+            
+        </div>
+    );
+};
+
+export default Home;
